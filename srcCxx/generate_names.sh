7ae124b9748a09b2a2ea22800506ba666ed1172a
ariasmartinez <44967058+ariasmartinez@users.noreply.github.com>
2022-10-20 13:21:18 +0200

    Update generate_names.sh

--- srcCxx/generate_names.sh
+++ srcCxx/generate_names.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-find ./ -type f -name "*" -not -name "*.o" -exec sh -c '
-    case "$(head -n 1 "$1")" in
-      ?ELF*) exit 0;;
-      MZ*) exit 0;;
-      #!*/ocamlrun*)exit0;;
-    esac
-exit 1' sh {} \; -fprint executables_names.txt
+echo "hola"
